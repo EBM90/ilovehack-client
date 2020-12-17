16de6c209102be88e6d1a6a5c48ec320ee720971
jose luis <joseluisblascoortiz@hotmail.com>
2020-12-17 10:59:14 +0100

    Fuuuuuusion

--- ilovehack/src/App.js
+++ ilovehack/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+
+import {Switch, Route} from 'react-router-dom'
+
+import AnonRoute from "./components/componentRoutes/AnonRoute";
+import PrivateRoute from "./components/componentRoutes/PrivateRoute";
+
+
+import Home from './components/Home/Home.jsx'
+import Events from './components/Events/Events.jsx'
+import EventDetail from './components/EventDetails/EventDetails.jsx'
+import Error from './components/ErrorPage/Error.jsx'
+import FAQ from './components/FAQ/FAQ.jsx'
+import Onboarding from './components/Onboarding/Onboarding';
+import Onboarding2 from './components/Onboarding2/Onboarding2';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Switch>
+      <Route exact path='/' component={Home}/>
+      <Route exact path='/events' component={Events}/>
+      <Route exact path='/event/:id' component={EventDetail}/>
+      <Route exact path='/error' component={Error}/>
+      <Route exact path='/FAQ' component={FAQ}/>
+      <Route exact path='/onboarding' component={Onboarding}/>
+      <Route exact path='/onboarding2' component={Onboarding2}/>
+     </Switch>
     </div>
   );
 }
