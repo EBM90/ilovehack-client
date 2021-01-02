7da91ab597a7a71d1543e0553e6f7183aa03d628
martacamacho <martacamachovargas@gmail.com>
2021-01-02 20:41:38 +0100

    attend event checkbox when editing is working now

--- ilovehack/src/pages/landing.jsx
+++ ilovehack/src/pages/landing.jsx
@@ -1,7 +1,8 @@
 import { Component } from "react";
 import React from "react";
 import eventservice from "../lib/event-service.js";
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+import { withAuth } from '../lib/AuthProvider';
 
 class Home extends Component {
     state = {
@@ -50,4 +51,4 @@ class Home extends Component {
         )
     }
 }
-export default Home;
+export default withAuth(Home);
