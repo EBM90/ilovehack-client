8343c26f0b00e86bf4996b08028f078d73d289c5
jose luis <joseluisblascoortiz@hotmail.com>
2021-01-03 12:30:46 +0100

    login css 88%

--- ilovehack/src/components/LogIn/Log-in.jsx
+++ ilovehack/src/components/LogIn/Log-in.jsx
@@ -22,18 +22,24 @@ class Login extends Component {
 
     return (
       <div className="login">
-        <h1 className="login-title">Log In</h1>
+        <h1 className="login-title">HacKonnect</h1>
 
         <form className="login-form" onSubmit={this.handleFormSubmit}>
           
-          <input className="border" type="email" name="email" value={email} placeholder="Username" onChange={this.handleChange}/>
+          <input className="border1" type="email" name="email" value={email} placeholder="Username" onChange={this.handleChange}/>
 
-          <input className="border" type="password" name="password" value={password} placeholder="Password" onChange={this.handleChange} />
+          <input className="border1" type="password" name="password" value={password} placeholder="Password" onChange={this.handleChange} />
+
+<div className="align-remember">
+          <input type="checkbox" name="remember pass" />
+          <p>Remember me</p>
+          <a className="forgotpass" href="#">Forgot your password?</a>
+          </div>
 
           <div className="login-button-div">
-          <input className="btn-login2" type="submit" value="Log in" />
+          <input className="btn-login2" type="submit" value="Log In" />
           </div>
-          <p>Don't have an account? <Link to={"/signup"}> Sign up</Link></p>
+          <p className="signup-login">Doesn´t have an account yet?<i className="suhere"> <Link to={"/signup"}> Sign up here!</Link></i> </p>
         </form>
       </div>
     );
